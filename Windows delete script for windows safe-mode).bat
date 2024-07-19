171cbe2cd0a249625f49d1da0a9409ef001d47bd
Suho Song <sh-@daum.net>
2024-07-20 00:32:22 +0900

    Create Windows_script
    Windows delete script for windows safe-mode)

---
+++ Windows delete script for windows safe-mode).bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Set Crowdstrike directory
+set "target_dir=C:\Windows\System32\drivers\CrowdStrike"
+
+:: Move to crowdstrike driver directory
+cd /d "%target_dir%"
+
+:: Delete the target patch files
+for %%F in (C-00000291*) do (
+    echo Deleting file: %%F
+    del "%%F"
+)
+
+echo File deletion complete
+pause
